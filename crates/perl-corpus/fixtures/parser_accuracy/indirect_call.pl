301e61370196b14b5fcd95dc1993158d76fd9cba
Steven Zimmerman, CPA <15812269+EffortlessSteven@users.noreply.github.com>
2026-08-10 20:17:47 -0400

    test(corpus): add indirect-call parser fixture

---
+++ crates/perl-corpus/fixtures/parser_accuracy/indirect_call.pl
@@ -0,0 +1,23 @@
+package Accuracy::IndirectCall;
+
+sub move {
+    my ($player, @steps) = @_;
+    return ($player, @steps);
+}
+
+sub run {
+    my $player = "Ada";
+    my $fh = *STDOUT;
+
+    move $player, 10, 20;
+    print $fh "ready\\n";
+    new Accuracy::IndirectCall::Runner "Ada";
+
+    if ($player) {
+        move $player, 1;
+    }
+
+    move($player, 30);
+}
+
+1;
