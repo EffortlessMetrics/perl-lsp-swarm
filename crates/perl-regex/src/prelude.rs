929bb397bd9405eb06aba0d4af1458783e217694
Steven Zimmerman, CPA <15812269+EffortlessSteven@users.noreply.github.com>
2026-08-11 23:48:47 -0400

    feat(perl-regex): export modifier model from prelude

--- crates/perl-regex/src/prelude.rs
+++ crates/perl-regex/src/prelude.rs
@@ -1,5 +1,11 @@
 pub use crate::{
     CaptureGroup, RegexAnalyzer, RegexError, RegexValidator,
+    analyzer::{
+        CaptureMode, CharacterSetMode, EffectiveModifiers, ExtendedMode, FeatureState,
+        ModifierAnalysis, ModifierRequirement, ModifierRequirementKind, ModifierSequence,
+        ModifierToken, PerlVersion, RegexLanguageProfile, RegexOperator, RequirementDisposition,
+        TransliterationModifiers,
+    },
     validator::{
         EmbeddedCodeFact, EmbeddedCodeKind, RegexAnalysis, RegexAnalysisCompleteness,
         RegexDiagnostic, RegexDiagnosticClass, RegexDiagnosticCode, RegexFacts, RegexRange,
