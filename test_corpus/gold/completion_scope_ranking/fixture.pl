74c6a22bc7864a32d65c4cb1e65165e2eb3879c3
Steven Zimmerman, CPA <15812269+EffortlessSteven@users.noreply.github.com>
2026-08-29 21:48:16 -0400

    test(editor): make scope ranking discriminate order (#13945)

--- test_corpus/gold/completion_scope_ranking/fixture.pl
+++ test_corpus/gold/completion_scope_ranking/fixture.pl
@@ -1,5 +1,9 @@
-my $scope_outer = 1;
+my $scope_a_ancestor = 1;
 {
-    my $scope_inner = 2;
-    my $x = $scope
+    my $scope_0_sibling = 2;
+}
+{
+    my $scope_z_local = 3;
+    my $x = $scope_
+    my $scope_1_future = 4;
 }
